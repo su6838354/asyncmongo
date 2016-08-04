725d8cfbe356c5114c0e2bf7a10d0d86999f90b1
suyuan <55Haitao@suyuan>
2016-08-04 10:50:18 +0800

    fix bson.decode_all invoke

--- asyncmongo/helpers.py
+++ asyncmongo/helpers.py
@@ -47,7 +47,7 @@ def _unpack_response(response, cursor_id=None, as_class=dict, tz_aware=False):
     result["cursor_id"] = struct.unpack("<q", response[4:12])[0]
     result["starting_from"] = struct.unpack("<i", response[12:16])[0]
     result["number_returned"] = struct.unpack("<i", response[16:20])[0]
-    result["data"] = bson.decode_all(response[20:], as_class, tz_aware)
+    result["data"] = bson.decode_all(response[20:])
     assert len(result["data"]) == result["number_returned"]
     return result
 
